f712129d4ec1b8370ca4337a7b195a9e5b5abd44
Abhishek Shrivastav <55507908+2023PHOENIX@users.noreply.github.com>
2022-10-11 16:03:56 +0530

    Time: 118 ms (41.94%), Space: 12.1 MB (17.06%) - LeetHub

---
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[(int)1e4 + 1][101];
+    int dfs(vector<int> &houses,int index,int k){
+
+        if(index == houses.size())return 0;
+
+        if(k == 0)return (int)1e9;
+        
+        if(dp[index][k] != -1)return dp[index][k];
+
+        int ans = INT_MAX;
+        for(int i = index;i<houses.size();i++){
+            
+            int median = houses[(index + i)/2];
+
+            int cost = 0;
+
+            for(int j = index;j<=i;j++){
+                cost += abs(median - houses[j]);
+            }
+
+            ans = min(ans,cost + dfs(houses,i + 1,k - 1));
+        }
+        return dp[index][k] = ans;
+    }
+    int minDistance(vector<int>& houses, int k) {
+        sort(houses.begin(),houses.end());
+        memset(dp,-1,sizeof(dp));
+        return dfs(houses,0,k);
+    }
+};
